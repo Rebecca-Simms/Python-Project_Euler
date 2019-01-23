da66e1b4f4f045042c0bbc279cb8f2b57a077abc
rebecca.simms <simmsr@thehutgroup.com>
2019-01-23 13:24:34 +0000

    Adding Project Euler Problem 10

---
+++ Problem_10/Problem10_Solution.py
@@ -0,0 +1,60 @@
+import time
+
+def SieveOfEratosthenes(n):
+    # Create a boolean array "prime[0..n]" and initialize
+    # all entries it as true. A value in prime[i] will
+    # finally be false if i is not a prime, else true.
+    prime = [True for i in range(n + 1)]
+    prime2 = []
+    p = 2
+    while (p * p <= n):
+        # If prime[p] is not changed, then it is a prime
+        if prime[p] == True:
+            # Update all multiples of p to False
+            for i in range(p * 2, n + 1, p):
+                prime[i] = False
+        p += 1
+    # return all prime numbers (all values which are still True)
+    for p in range(2, n):
+        if prime[p]: # True
+            prime2.append(p)
+    return prime
+
+def sum_of_primes(p):
+    summation = [0] * len(p)
+    summation[0] = summation[1] = 0
+    summation[2] = 2
+    for i in range(3, len(summation)):
+        if p[i] is True:
+            summation[i] = summation[i-1] + i
+        else:
+            summation[i] = summation[i-1]
+    return summation
+
+
+
+# Checking how long algorithm takes
+
+start_time = time.clock()
+
+primes = SieveOfEratosthenes(1000000)
+
+s = sum_of_primes(primes)
+
+# test cases
+
+print(s[5])
+print(s[10])
+print(s[100])
+print(s[34536])
+
+print(time.clock() - start_time, "seconds")  # 0.372073 seconds
+
+###
+
+def find_sum(i):
+    s = sum_of_primes(primes)
+    return s[i]
+
+def test_answer():
+    assert find_sum(10) == 17
